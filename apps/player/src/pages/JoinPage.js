ec64c09305f4e1483f9b2f97488d3361e2f91aa1
rook701010-commits <rook701010@gmail.com>
2026-07-18 05:26:57 +0900

    chore: prevent TypeScript build artifacts from being emitted

--- apps/player/src/pages/JoinPage.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import JoinForm from "../components/JoinForm";
-export default function JoinPage(props) {
-    return _jsx(JoinForm, { ...props });
-}
